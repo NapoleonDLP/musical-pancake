7776c3a5e0349668f021b3befa64e74d496b157b
NapoleonDLP <napoleon.dlp@gmail.com>
2022-02-18 21:37:13 -0800

    Solution

--- prompt-3.js
+++ prompt-3.js
@@ -1,16 +1,50 @@
 
 const solution = (T, R) => {
   // declare pointsCount
+  let pointsCount = 0;
 
   // get length of group string without the a, b, c
+  let sortedByStringsLength = T.sort((a, b) => a.length - b.length);
+  let groupStringsLength = sortedByStringsLength[0].length;
 
   // store data in an obj where key is the group and value is an array [amount of times group is tested, amount of 'OK]
+  let collection = {};
+  let totalGroups = 0;
+
+  for(let i = 0; i < T.length; i++) {
+    let currentTestString = T[i].substring(0, groupStringsLength);
+
+    if (collection[currentTestString] === undefined) {
+      collection[currentTestString] = [1, 0];
+      totalGroups++;
+
+      if (R[i] === 'OK') {
+        collection[currentTestString][1]++;
+      }
+
+    } else if (collection[currentTestString]) {
+      collection[currentTestString][0]++;
+
+      if (R[i] === 'OK') {
+        collection[currentTestString][1]++;
+      }
+    }
+  };
 
   // iterate through object
+  for (let testGroup in collection) {
     // if object [testGroup][0] is equal to object[testGroup][1]
+    console.log(collection[testGroup])
+    if (collection[testGroup][0] === collection[testGroup][1]) {
       // increase pointsCount by 1
+      pointsCount++;
+    }
+  }
 
   // return points count / total groups rounded down. Math.floor()
+  const roundedAvg = Math.floor(((pointsCount * 100) / totalGroups));
+
+  return roundedAvg;
 };
 
 /*
@@ -32,6 +66,6 @@ const arg4 = ['Wrong answer', 'OK', 'OK', 'Runtime error', 'OK'];
 const test2 = solution(arg3, arg4); //Solution: 50
 
 console.log(
-  "Test 1: " + (test1 === 33 ? `pass: expected ${ 33 } got ${test1}` : `fail: expected ${ 33 }} got ${test1}`: "fail"),
-  "\nTest 2: " + (test2 === 50 ? `pass: expected ${ 50 } got ${test2}` : `fail: expected ${ 50 }} got ${test2}`: "fail")
+  "Test 1: " + (test1 === 33 ? `pass: expected ${ 33 } got ${test1}` : `fail: expected ${ 33 }} got ${test1}`),
+  "\nTest 2: " + (test2 === 50 ? `pass: expected ${ 50 } got ${test2}` : `fail: expected ${ 50 }} got ${test2}`)
 )
